b95cee70868e130d7aeb24732921ccb4c07e0713
alexisioa629 <alexisioa629@gmail.com>
2020-02-24 18:52:47 -0500

    2020-02-24

--- src/vista/js/main.js
+++ src/vista/js/main.js
@@ -1,35 +1,34 @@
 //ejecutar jquery
-$(function(){
-console.log('jquery funcionando');
-const socket= io();
+$(function () {
+    console.log('jquery funcionando');
+    const socket = io();
 
-//obtener los elementos del html
-const htmlMensajeForm=$('#mensaje-form');
-const htmlMensajeCuadro=$('#mensaje');
-const htmlChat=$('#chat');
-const htmlTxtNombre=$('#txtNombre');
+    //obtener los elementos del html
+    const htmlMensajeForm = $('#mensaje-form');
+    const htmlMensajeCuadro = $('#mensaje');
+    const htmlChat = $('#chat');
+    const htmlTxtNombre = $('#txtNombre');
 
-//obtener hora
-var fecha= new Date();
-var hora = fecha.getHours();
-var minutos= fecha.getMinutes();
 
-//Eventos de html
-htmlMensajeForm.submit(e=>{
-    e.preventDefault(); // evitar que se recargue la pagina
-    
-    //evento para enviar al servidor sms::::nombre del evento+ de donde lo obtengo
-    socket.emit('mensaje desde cliente',`${htmlTxtNombre.val()}: ${htmlMensajeCuadro.val()}`);
-    htmlMensajeCuadro.val('');
-});
 
-//mostrar los datos a los usuarios
-socket.on('mensaje desde servidor',function(data){
-    //htmlChat.append(hora+minutos+''+data+'<br>');
-    htmlChat.append(`${hora}:${minutos}<br>${data}<br>`);
-})
+    //Eventos de html
+    htmlMensajeForm.submit(e => {
+        e.preventDefault(); // evitar que se recargue la pagina
+
+        //evento para enviar al servidor sms::::nombre del evento+ de donde lo obtengo
+        socket.emit('mensaje desde cliente', `${htmlTxtNombre.val()}:<br>"${htmlMensajeCuadro.val()}"`);
+        htmlMensajeCuadro.val('');
+    });
+
+    //mostrar los datos a los usuarios
+    socket.on('mensaje desde servidor', function (data) {
+        //obtener hora
+        var fecha = new Date();
+        var hora = fecha.getHours();
+        var minutos = fecha.getMinutes();
+        htmlChat.append(`<br>${data}<br>${hora}:${minutos}<br>`);
+    })
 
 });
 //alexis
 
-    
